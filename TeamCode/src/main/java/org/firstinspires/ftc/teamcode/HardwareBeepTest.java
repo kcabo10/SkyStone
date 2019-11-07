a26d03976217f660316c6371d17ef14cecb0a14e
Kylie <kylie1234@cox.net>
2019-11-07 09:38:57 -0700

    RedFoundation and BlueFoundation both need to be tested with the competition bot. I have not put in the code for the foundation hooks but the code drives to the points on both the blue side and the red side. The parking will be reliant on the final position after the robot pushes the foundation into the corner. Again this needs to be tested with the competition bot to account for any new variables introduced by dragging the foundation. I also disabled the programs we will not be using for this competition (as far as I know). This can be undone by commenting out the @Disabled line of code. I also deleted any un necessary hardware maps to clean up our repository.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareBeepTest.java
@@ -0,0 +1,93 @@
+//Declare package
+package org.firstinspires.ftc.teamcode;
+
+//Import Hardware
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.sensors.SensorMB1242;
+
+/**
+ * @author Beep Patrol
+ * <p>
+ * <b>Summary:</b>
+ * <p>
+ * This is our Hardware Map that contains all the motors, servos, and sensors that we use on the
+ * robot. We pull this Hardware Map in all the programs we use a part of the robot. In this program
+ * we intialize the encoders on the motors we want to call the encoder for.
+ */
+public class HardwareBeepTest {
+
+    // Set Public OpMode Members
+    public DcMotor leftFront = null;
+    public DcMotor leftBack = null;
+    public DcMotor rightFront = null;
+    public DcMotor rightBack = null;
+    public SensorMB1242 rightSonic = null;
+    public SensorMB1242 leftSonic = null;
+    public BNO055IMU imu = null;
+
+    // Set local OpMode Members
+    HardwareMap hwMap = null;
+    private ElapsedTime period = new ElapsedTime();
+
+    // Constructor
+    public HardwareBeepTest() {
+    }
+
+    /**
+     * Initializes standard hardware interfaces
+     *
+     * @param ahwMap A reference to Hardware Map
+     */
+    public void init(HardwareMap ahwMap) {
+
+        // Telemetry Switches
+        boolean GRID_NAV_TELEMETRY_ON = true;
+
+        // Save Reference To Hardware Map
+        hwMap = ahwMap;
+
+        // Define Motors, Servos, and Sensors
+        leftFront = hwMap.get(DcMotor.class, "left_front");
+        leftBack = hwMap.get(DcMotor.class, "left_back");
+        rightFront = hwMap.get(DcMotor.class, "right_front");
+        rightBack = hwMap.get(DcMotor.class, "right_back");
+        leftSonic = hwMap.get(SensorMB1242.class, "left_sonic");
+        rightSonic = hwMap.get(SensorMB1242.class, "right_sonic");
+        imu = hwMap.get(BNO055IMU.class, "imu");
+
+        // Set Motor and Servo Direction
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+
+
+        // Set Motor to Zero Power Behavior
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        // Set Motors to Run Without Encoders
+        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Set IMU Parameters
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = false;
+
+        // Initialize IMU
+        imu.initialize(parameters);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedFoundation.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name = "Red Foundation", group = "Beep")
+public class RedFoundation extends LinearOpMode {
+
+    // Declaring a timer
+    public ElapsedTime runtime = new ElapsedTime();
+    // Calling hardware map
+    HardwareBeep robot = new HardwareBeep();
+
+    LibraryGyroDrive gyroDrive = new LibraryGyroDrive();
+
+    LibraryGyro gyroTurn = new LibraryGyro();
+
+    @Override
+    public void runOpMode() {
+
+        telemetry.addData("Telemetry", "robot initializing");
+        telemetry.update();
+        //initializing the hardware map
+        robot.init(hardwareMap);
+        gyroDrive.init(robot, telemetry, robot.rightBack);
+        gyroTurn.init(robot, telemetry);
+        telemetry.addData("Telemetry", "run opMode start");
+        telemetry.update();
+
+        //wait for start
+        waitForStart();
+
+        robot.leftFront.setPower(-0.5);
+        robot.rightFront.setPower(0.5);
+        robot.leftBack.setPower(0.5);
+        robot.rightBack.setPower(-0.5);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 2.7)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        sleep(500);
+
+        robot.leftFront.setPower(0.5);
+        robot.rightFront.setPower(0.5);
+        robot.leftBack.setPower(0.5);
+        robot.rightBack.setPower(0.5);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < .3)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        sleep(500);
+
+        gyroTurn.turnGyro(-60);
+
+        gyroDrive.gyroDrive(0.5, 1500, 0);
+    }
+}
