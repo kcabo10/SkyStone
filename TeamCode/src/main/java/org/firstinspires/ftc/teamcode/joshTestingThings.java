ec0843ee3d9b276d94c44dd635e7da01337ee6d2
lawillett <oogorg@yahoo.com>
2020-01-04 13:37:54 -0700

    added parkingUnderBridge and joshTestingThings

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/joshTestingThings.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * @author Beep Patrol
+ * <p>
+ * <b>Summary:</b>
+ * <p>
+ * This is our autonomous program for the depot side on the blue side of the field. This program runs
+ * without the phone light for Tensor Flow. This is the go to program. This program... .
+ */
+
+@Autonomous(name = "Josh testing things", group = "Beep")
+public class joshTestingThings extends LinearOpMode {
+
+    // Declaring a timer
+    public ElapsedTime runtime = new ElapsedTime();
+    public String foundTargetName = "";
+    //Calling our hardware map
+    HardwareBeep robot = new HardwareBeep();
+    // Calling the Library Gyro program to use the gyro turn function
+    LibraryGyro gyroTurn = new LibraryGyro();
+    // Calling the Library Gyro Drive program to use the gyro drive function
+    LibraryGyroDrive gyroDrive = new LibraryGyroDrive();
+
+    LibraryGridNavigation gridNavigation = new LibraryGridNavigation();
+
+    /**
+     * This method is the main body of our code which contains the set of commands carried out in our crater side autonomous program.
+     */
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Telemetry", "robot initializing");
+        telemetry.update();
+        //initializing the hardware map
+        robot.init(hardwareMap);
+        //initializing the gyro turn function
+        gyroTurn.init(robot, telemetry);
+        //initializing the gyro drive function
+        gyroDrive.init(robot, telemetry, robot.rightBack);
+        gridNavigation.init(robot, gyroTurn, telemetry);
+        telemetry.addData("Telemetry", "run opMode start");
+        telemetry.update();
+
+        waitForStart();
+
+        gridNavigation.setGridPosition(1, 0.4,0);
+
+        gridNavigation.driveToPosition(4, 0.4, .5);
+
+    robot.leftFront.setPower(.5);
+    robot.leftBack.setPower(.5);
+    robot.rightFront.setPower(.5);
+    robot.rightBack.setPower(.5);
+
+    }
+}
